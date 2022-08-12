24b3cba9700141c52905e43d196530655350038b
Sean O'Connor <soconnor@insperex.com>
2022-08-11 17:20:15 -0700

    Package sync

--- test/OpenTelemetry.Instrumentation.MassTransit.Tests/MassTransitInstrumentationTests.cs
+++ test/OpenTelemetry.Instrumentation.MassTransit.Tests/MassTransitInstrumentationTests.cs
@@ -18,6 +18,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using MassTransit.Testing;
 using Moq;
 using OpenTelemetry.Instrumentation.MassTransit.Implementation;
